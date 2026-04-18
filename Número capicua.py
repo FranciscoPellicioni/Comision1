fad292c6556940046b64e5541d06694bfe354138
FranciscoPellicioni <franciscopellicioni8@gmail.com>
2026-04-17 22:27:33 -0300

    Ejercicio corregido

--- Número capicua.py
+++ Número capicua.py
@@ -1,34 +1,17 @@
-num = input()
-
-# Eliminar espacios al inicio y al final
-num = num.strip()
-
-# Caso: no ingresa nada o solo espacios
-if num == "":
-    print("Error en el ingreso de los datos")
-
-# Caso: no es un número
-elif not (num.isdigit() or (num.startswith("-") and num[1:].isdigit())):
-    print("Error en el ingreso de los datos")
-
-else:
-    num = int(num)
-
-    # Caso 2: número negativo
-    if num < 0:
-        print("El numero debe ser mayor a 0")
-
-    # Caso 3: no tiene 3 cifras
-    elif num < 100 or num > 999:
-        print("El numero de ser de 3 cifras")
-
+try:
+    numero = int(input (""))
+    if numero < 1:
+        print ("El numero debe ser mayor a 0")
     else:
-        # Obtener centenas y unidades
-        centena = num // 100
-        unidad = num % 10
-
-        # Verificar si es capicúa
-        if centena == unidad:
-            print("Es capicua")
+        if numero < 100 or numero > 999:
+          print ("El numero de ser de 3 cifras")
         else:
-            print("No es capicua")
+            centena = numero // 100 % 10
+            decena = numero //10 % 10
+            unidad = numero % 10
+            if centena == unidad:
+             print ("Es capicua")  
+            else:
+                print ("No es capicua")  
+except ValueError:
+   print ("Error en el ingreso de los datos")
